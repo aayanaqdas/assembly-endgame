ecf01c9baa26bec026286e5fd0bfe4d350df4530
aayanaqdas <aayan.aqdas@outlook.com>
2025-08-09 04:09:43 +0200

    implement game status updates and farewell messages; enhance styles for game status display

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
 import { useState } from "react";
 import { clsx } from "clsx";
 import { languages } from "./languages";
+import { getFarewellText } from "./utils";
 
 export default function AssemblyEndgame() {
   const [currWord, setCurrWord] = useState("react");
   const [guessedLetters, setGuessedLetters] = useState([]);
 
-  console.log(guessedLetters);
+  const wrongGuessCount = guessedLetters.filter((letter) => !currWord.includes(letter)).length;
+
+  const isGameLost = wrongGuessCount >= languages.length - 1;
+  const isGameWon = currWord.split("").every((letter) => guessedLetters.includes(letter));
+  const isGameOver = isGameWon || isGameLost;
+
+  const lastGuessedLetter = guessedLetters[guessedLetters.length - 1];
+  const isLastGuessWrong = lastGuessedLetter && !currWord.includes(lastGuessedLetter);
+
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-  const languagesElements = languages.map((language) => {
+  const languageElements = languages.map((lang, index) => {
+    const isLost = index < wrongGuessCount;
+
+    const styles = {
+      backgroundColor: lang.backgroundColor,
+      color: lang.color,
+    };
     return (
-      <span
-        key={language.name}
-        style={{ backgroundColor: language.backgroundColor, color: language.color }}
-      >
-        {language.name}
+      <span className={clsx("chip", { lost: isLost })} style={styles} key={lang.name}>
+        {lang.name}
       </span>
     );
   });
@@ -34,16 +46,56 @@ export default function AssemblyEndgame() {
     const isWrong = isGuessed && !currWord.includes(letter);
     const className = clsx({ "right-letter": isRight, "wrong-letter": isWrong });
     return (
-      <button className={className} key={letter} onClick={() => addGuessedLetters(letter)}>
+      <button
+        className={className}
+        key={letter}
+        disabled={isGameOver}
+        onClick={() => addGuessedLetters(letter)}
+      >
         {letter}
       </button>
     );
   });
 
   function addGuessedLetters(letter) {
-    setGuessedLetters((prevLetter) =>
-      prevLetter.includes(letter) ? prevLetter : [...prevLetter, letter]
-    );
+    if (!isGameOver) {
+      setGuessedLetters((prevLetter) =>
+        prevLetter.includes(letter) ? prevLetter : [...prevLetter, letter]
+      );
+    }
+  }
+
+  const gameStatusClass = clsx("game-status", {
+    won: isGameWon,
+    lost: isGameLost,
+    farewell: !isGameOver && isLastGuessWrong,
+  });
+
+  function renderGameStatus() {
+    if (!isGameOver && isLastGuessWrong) {
+      return (
+        <p className="farewell-message">{getFarewellText(languages[wrongGuessCount - 1].name)}</p>
+      );
+    }
+
+    if (isGameWon) {
+      return (
+        <>
+          <h2>You win!</h2>
+          <p>Well done! 🎉</p>
+        </>
+      );
+    }
+    if (isGameLost) {
+      return (
+        <>
+          <h2>Game over!</h2>
+          <p>You lose! Better start learning Assembly 😭</p>
+        </>
+      );
+    }
+
+    return null;
   }
   return (
     <main>
@@ -51,14 +103,11 @@ export default function AssemblyEndgame() {
         <h1>Assembly: Endgame</h1>
         <p>Guess the word within 8 attempts to keep the programming world safe from Assembly!</p>
       </header>
-      <section className="game-status">
-        <h2>You win!</h2>
-        <p>Well done! 🎉</p>
-      </section>
-      <section className="languages-container">{languagesElements}</section>
+      <section className={gameStatusClass}>{renderGameStatus()}</section>
+      <section className="language-chips">{languageElements}</section>
       <section className="word-container">{word}</section>
       <section className="keyboard-container">{keyboard}</section>
-      <button className="new-game">New Game</button>
+      {isGameOver && <button className="new-game">New Game</button>}
     </main>
   );
 }
